ecd233dec8d80176bc247ec4c13085932fe908b2
aniket2727 <aniketkadam5950@gmail.com>
2024-10-05 23:53:10 +0530

    add cod e

---
+++ project-name/src/component/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  name: string = '';  // Class properties for name and email
+  email: string = '';
+
+  handleLogin() {
+    console.log('The name and email:', this.name, this.email);
+  }
+}
