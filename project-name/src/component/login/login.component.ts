2199a7d31ed92af7c85f88ac7bb266f47640deb3
aniket2727 <aniketkadam5950@gmail.com>
2024-10-05 23:54:55 +0530

    add code

--- project-name/src/component/login/login.component.ts
+++ project-name/src/component/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Import FormsModule
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
